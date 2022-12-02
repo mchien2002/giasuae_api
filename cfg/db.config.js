3cf7237ddbc4bf255ce4146de1a853e6a294c09a
mchien2002 <minhchien77777@gmail.com>
2022-12-02 16:54:23 +0700

    return commit

--- cfg/db.config.js
+++ cfg/db.config.js
@@ -9,24 +9,11 @@ const db = mysql.createConnection({
     multipleStatements: true
 });
 
-function handleDisconnect(){
-    db.connect((error) => {
-        if (error) {
-            console.log("**** ERROR ****");
-            console.log(error);
-            setTimeout(handleDisconnect, 2000);
-        }
-    });
-    db.on('error', function (err) {
-        console.log('db error', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
-        }
-    });
-}
-
-handleDisconnect();
+db.connect((error) => {
+    if (error) {
+        console.log("**** ERROR ****");
+        console.log(error);
+    }
+});
 
 module.exports = db;
